a6151810760ddc23b823eb72c1a4bd42c80eae49
Muhammad Abbas <abbas@Muhammads-MacBook-Pro.local>
2019-05-21 10:23:34 +0400

    properties cleaned and burst operation

--- src/main/java/com/example/OrderController.java
+++ src/main/java/com/example/OrderController.java
@@ -25,4 +25,18 @@ public class OrderController {
         return orderRepository.findById(orderId).orElse(null);
     }
 
+    @GetMapping("/burst")
+    public String burst() {
+        for (int i = 0; i < 10000; i++) {
+            ExchangeOrder order = new ExchangeOrder();
+            order.setAmount(33.3f);
+            order.setUserId(5l);
+            order.setOrderId(i);
+            orderRepository.save(order);
+            orderRepository.findAll();
+        }
+        orderRepository.deleteAll();
+        return "Done";
+    }
+
 }
